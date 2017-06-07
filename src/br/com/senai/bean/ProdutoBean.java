5576d5b4edf82f916757bf3553c0a788bdab6383
Equipe3 e Makita <osvaldo_cabral@outlook.com.br>
2017-06-07 11:50:46 -0300

    Fornecedor no produto(falta terminar)

---
+++ src/br/com/senai/bean/ProdutoBean.java
@@ -0,0 +1,67 @@
+package br.com.senai.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+import br.com.senai.dao.FornecedorDAO;
+import br.com.senai.dao.ProdutoDAO;
+import br.com.senai.model.Fornecedor;
+import br.com.senai.model.Produto;
+
+@ManagedBean
+@SessionScoped
+public class ProdutoBean {
+
+	private Produto produto = new Produto();
+	private List<Produto> produtos = new ArrayList<Produto>();
+	private List<Fornecedor> fornecedores;
+	
+	public ProdutoBean(){
+		produtos = new ProdutoDAO().listarProdutos();
+		fornecedores = new FornecedorDAO().listarFornecedores();
+	}
+	
+	public String salvar(){
+		new ProdutoDAO().salvar(produto);
+		//produtos.add(produto);
+		produtos = new ProdutoDAO().listarProdutos();
+		produto = new Produto();
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Produto salvo com sucesso!"));
+		return "produtolist_template?faces-redirect=true";
+	}
+	
+	public String editar(Produto produto){
+		this.produto = produto;
+		return "produtocad_template?faces-redirect=true";
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+
+	public List<Fornecedor> getFornecedores() {
+		return fornecedores;
+	}
+
+	public void setFornecedores(List<Fornecedor> fornecedores) {
+		this.fornecedores = fornecedores;
+	}
+	
+}
