e9ee9e4c79d4909c9d5081c4f8bb6007c70305dc
osvaldocabral <osvaldo_cabral@outlook.com.br>
2017-06-26 15:54:16 -0300

    Tudo de contas a pagar

---
+++ src/br/com/senai/bean/ContasPagarBean.java
@@ -0,0 +1,55 @@
+package br.com.senai.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+import br.com.senai.dao.ContasPagarDAO;
+import br.com.senai.model.ContasPagar;
+
+@ManagedBean
+@SessionScoped
+public class ContasPagarBean {
+
+	private ContasPagar contasPagar = new ContasPagar();
+	private List<ContasPagar> listContasPagar = new ArrayList<ContasPagar>();
+
+	public ContasPagarBean() {
+		listContasPagar = new ContasPagarDAO().listarContas();
+	}
+
+	public String salvar() {
+		new ContasPagarDAO().salvar(contasPagar);
+		listContasPagar = new ContasPagarDAO().listarContas();
+		contasPagar = new ContasPagar();
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Conta a pagar salva!"));
+		return "contaspagarlist_template";
+	}
+	
+	public void excluir(ContasPagar contasPagar) {
+		new ContasPagarDAO().excluir(contasPagar);
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Conta a pagar excluida!"));
+		listContasPagar = new ContasPagarDAO().listarContas();
+	}
+
+	public ContasPagar getContasPagar() {
+		return contasPagar;
+	}
+
+	public void setContasPagar(ContasPagar contasPagar) {
+		this.contasPagar = contasPagar;
+	}
+
+	public List<ContasPagar> getListContasPagar() {
+		return listContasPagar;
+	}
+
+	public void setListContasPagar(List<ContasPagar> listContasPagar) {
+		this.listContasPagar = listContasPagar;
+	}
+
+}
