16de82ec9faefa10619ddef90dadae7624e59614
Luan <luan3642@hotmail.com>
2017-06-26 13:27:36 -0300

    Cliente feito o javaDoc

--- src/br/com/senai/bean/ClienteBean.java
+++ src/br/com/senai/bean/ClienteBean.java
@@ -5,34 +5,45 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
-
 import br.com.senai.dao.ClienteDao;
 import br.com.senai.model.Cliente;
 import br.com.senai.model.Estado;
 import br.com.senai.model.SexoCliente;
 
+/** 
+* @author Equipe 1 (Leonardo André, Luan Magalhães, Matheus Garcia, Vinicius Andrade)
+*/
+/**
+ * Classe para persistência de cliente: métodos de salvar, excluir e editar.
+ */
 @ManagedBean
 @SessionScoped
 public class ClienteBean {
-
 	private Cliente cliente = new Cliente();
 	private List<Estado> estados = Arrays.asList(Estado.values());
 	private List<SexoCliente> sexos = Arrays.asList(SexoCliente.values());
 	private List<Cliente> clientes = new ArrayList<Cliente>();
-	public Thread currentThread;
 
+	/**
+	 * Método construtor tem a finalidade de criar um objeto em memória.
+	 */
 	public ClienteBean() {
 		clientes = new ClienteDao().listarClientes();
 	}
 
+	/**
+	 * @return clientelist_template
+	 * @throws InterruptedException
+	 * 
+	 *             Método para salvar todas as informações de cliente.
+	 *             Retornando mensagem de sucesso.
+	 */
 	@SuppressWarnings("static-access")
 	public String salvar() throws InterruptedException {
-		Thread.currentThread().sleep(5000);
 		new ClienteDao().salvar(cliente);
 		clientes = new ClienteDao().listarClientes();
 		cliente = new Cliente();
@@ -40,21 +51,37 @@ public class ClienteBean {
 		return "clientelist_template";
 	}
 
+	/**
+	 * @param cliente
+	 * @return clientecad_template Método para editar, altera informações já
+	 *         cadastradas.
+	 */
 	public String editar(Cliente cliente) {
 		this.cliente = cliente;
 		return "clientecad_template";
 	}
 
+	/**
+	 * 
+	 * @param cliente
+	 *            Método prepararExclusao, responsável por colocar cliente
+	 *            selecionado na model.
+	 */
 	public void prepararExclusao(Cliente cliente) {
 		this.cliente = cliente;
 	}
 
+	// Método
 	public void excluir() {
 		new ClienteDao().excluir(cliente);
 		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("cliente deletado com sucesso!"));
 		clientes = new ClienteDao().listarClientes();
 	}
 
+	/**
+	 * Método para apanhar data atual do servidor.
+	 * 
+	 */
 	public String getDataAtual() {
 		return new SimpleDateFormat("dd/MM/yyyy").format(new Date());
 	}
