bdb9794566883462b6c450d6738b1d4077803e5e
stapafunico <leonardoandre.maia@gmail.com>
2017-06-26 11:12:11 -0300

    feito contas a receber funcionando

--- src/br/com/senai/dao/ContasReceberDAO.java
+++ src/br/com/senai/dao/ContasReceberDAO.java
@@ -1,5 +1,28 @@
 package br.com.senai.dao;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import br.com.senai.model.ContasReceber;
+
 public class ContasReceberDAO {
 
+	public void salvar(ContasReceber contasReceber){
+		EntityManager entityManager = JPAUtil.getEntityManager();
+		entityManager.getTransaction().begin();
+		entityManager.merge(contasReceber);
+		entityManager.getTransaction().commit();
+		entityManager.close();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<ContasReceber> listarContas(){
+		
+		EntityManager entityManager = JPAUtil.getEntityManager();
+		Query query = entityManager.createQuery("from ContasReceber Order By id");
+		return query.getResultList();
+	}
+	
 }
